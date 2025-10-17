a3c7dec7aab81ca703a85a748c370ee157a356a6
TonyCDalvin <83288223+TonyRDX@users.noreply.github.com>
2025-10-17 15:09:42 +0200

    test: form component

--- front/src/app/features/sessions/components/form/form.component.spec.ts
+++ front/src/app/features/sessions/components/form/form.component.spec.ts
@@ -1,12 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {  ReactiveFormsModule } from '@angular/forms';
+import {  FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
@@ -14,22 +14,48 @@ import { SessionService } from 'src/app/services/session.service';
 import { SessionApiService } from '../../services/session-api.service';
 
 import { FormComponent } from './form.component';
+import { of } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
-
-  const mockSessionService = {
-    sessionInformation: {
-      admin: true
-    }
-  } 
+  let mockSessionService: any;
+  let mockSnackBar: any;
+  let mockActivatedRoute: any;
+  let mockRouter: any;
+  let mockSessionApiService: any;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    mockSessionService = {
+      sessionInformation: {
+        admin: true
+      }
+    };
 
+    mockSnackBar = {
+      open: jest.fn()
+    };
+
+    mockSessionApiService = {
+      create: jest.fn().mockReturnValue(of({})),
+      update: jest.fn().mockReturnValue(of({})),
+      detail: jest.fn().mockReturnValue(of({}))
+    };
+
+    mockActivatedRoute = { 
+      snapshot: { 
+        paramMap: new Map([['id', '123']]) 
+      } 
+    };
+
+    mockRouter = {
+      navigate: jest.fn().mockResolvedValue(true),
+      url: '/sessions/create',
+    };
+
+    await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
         HttpClientModule,
         MatCardModule,
         MatIconModule,
@@ -42,7 +68,10 @@ describe('FormComponent', () => {
       ],
       providers: [
         { provide: SessionService, useValue: mockSessionService },
-        SessionApiService
+        { provide: SessionApiService, useValue: mockSessionApiService },
+        { provide: MatSnackBar, useValue: mockSnackBar },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter}, 
       ],
       declarations: [FormComponent]
     })
@@ -50,10 +79,41 @@ describe('FormComponent', () => {
 
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
+  });
+
+  it('should call create, open snackbar, and redirect', () => {
+    fixture.detectChanges();
+
+    component.submit();
+    expect(mockSessionApiService.create).toHaveBeenCalled();
+    expect(mockSnackBar.open).toHaveBeenCalled();
+    expect(mockRouter.navigate).toHaveBeenCalled();
+  });
+
+  it('should call update, open snackbar, and redirect', () => {
+    const fb = TestBed.inject(FormBuilder);
+    let mockFb = fb.group({
+      name: ['Angular', Validators.required],
+      date: ['2025-01-01', Validators.required],
+      teacher_id: ['t1', Validators.required],
+      description: ['Desc', [Validators.required, Validators.max(2000)]],
+    });
+    mockRouter.url = '/sessions/update';
+    mockSessionApiService.update = jest.fn().mockReturnValue(
+      of({mockFb})
+    );
+
+    fixture.detectChanges();
+    
+    component.submit();
+    expect(mockSnackBar.open).toHaveBeenCalledTimes(1);
+    expect(mockSessionApiService.update).toHaveBeenCalledTimes(1);
+    expect(mockRouter.navigate).toHaveBeenCalledTimes(1);
   });
 });
