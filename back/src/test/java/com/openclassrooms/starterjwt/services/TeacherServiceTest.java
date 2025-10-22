b69deb7e7265eb5818a05a2886c10dbfb05d350a
TonyCDalvin <83288223+TonyRDX@users.noreply.github.com>
2025-10-22 19:19:13 +0200

    chore: remove useless import

--- back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
